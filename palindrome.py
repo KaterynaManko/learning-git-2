5e838bf7acd8f9650a6c06090f3b6fad5bf87de3
KaterynaManko <k.manko16@gmail.com>
2023-01-01 22:24:22 +0200

    Add a function that checks if a word is a palindrome

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+def palindrome(word):
+    """Checks whether a word is a palindrome"""
+    letters_list = [letter for letter in word]
+    reversed_letters_list = letters_list[::-1]
+    if(letters_list==reversed_letters_list):
+        return True
+    return False
